1e7face5be3e9e39c9a72f765f9ef051fe9d0292
qun-tt <467552709@qq.com>
2020-10-28 22:33:40 +0800

    week5

---
+++ week5-2/5-5.c
@@ -0,0 +1,19 @@
+#include "./ch05.h"
+int main()
+{
+	char buf[20];
+	int ret;
+	FILE *fp=fopen("./tem.txt","w+");
+	if(!fp){
+		printf("Fail to open file!\n");
+		exit(-1);
+	}
+	ret =fwrite("123",sizeof("123"),1,fp);
+	printf("We write %d byte\n",ret);
+	memset(buf,0,sizeof(buf));
+	ret=fread(buf,1,1,fp);
+	printf("We read %s,ret is %d\n",buf,ret);
+	fwrite("456",sizeof("456"),1,fp);
+	fclose(fp);
+	return 0;
+}
---
+++ week5-2/ch05.h
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <string.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include<time.h>
+void err_exit(char *s)
+{
+	printf("USAGE:*s is faile!\n!");
+	exit(1);
+}
